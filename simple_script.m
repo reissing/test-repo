096f5ca49df2b33572bccfdb0fc170333e3661c0
reissing <ralf.reissing@hs-coburg.de>
2014-09-12 13:01:33 +0200

    interactive section added

--- simple_script.m
+++ simple_script.m
@@ -2,4 +2,12 @@
 
 clear  %clear command window
 
-disp('Hello, world!');
+disp('Hello, world!')
+
+lastname = input('Please enter your last name: ', 's');
+firstname = input('Please enter your first name: ', 's');
+
+fprintf('\nWelcome to Matlab, dear %s %s.\n\n', firstname, lastname)
+
+disp('End of script reached.');
+
